967cb3d23aa8b05ac974a8c6581a53c06963bd3f
CBickel87 <CBickel87@gmail.com>
2016-04-16 06:42:11 -0400

    Cisco IP phone search
    Needed to find Cisco phones on the network. Sensitive info redacted.

---
+++ ip_phone_search.py
@@ -0,0 +1,50 @@
+import re
+import requests
+import csv
+from netaddr import *
+from twilio.rest import TwilioRestClient
+
+
+iprange = (IPRange('beginning range', 'end range'))
+
+
+for ip in iprange:
+    try:
+        r = requests.get('http://' + str(ip), timeout=0.5)
+
+        print('---------------')
+        print(ip)
+        print(r.status_code)
+        print('---------------')
+
+        if r.status_code == 200:
+            verify_cisco = re.compile(r'(ip phone)', re.I)  # See if 'IP Phone' is on the page (Ignore case)
+            mo = verify_cisco.search(r.text)
+            print('*** I found one! ' + mo.group())
+
+            if mo is not None:
+                typeRegex = re.compile(r'(7942G|7962G|7911|7925)')  # Filtering out only 7942Gs,7962Gs, 7911s,7925s
+                mo2 = typeRegex.search(r.text)
+
+                if mo2 is not None:
+                    print(mo2.group())
+                    serialRegex = re.compile(r'FCH(\w){8}')  # Find Serial Number
+                    serial = serialRegex.search(r.text)
+                    print(serial.group())
+
+                    with open('IP_Phones.csv', 'a') as f:  # Saving serials in a csv
+                        writer = csv.writer(f, delimiter='|', lineterminator='\n')
+                        writer.writerow([serial.group()])
+
+    except Exception as err:
+        'There was an error'
+
+
+# Text me when complete
+accountSID = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+authToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+twilioCli = TwilioRestClient(accountSID, authToken)
+myTwilioNumber = '+xxxxxxxxxxx'
+myCellPhone = '+xxxxxxxxxxx'
+message = twilioCli.messages.create(body='IP Phone search completed',
+                                    from_=myTwilioNumber, to=myCellPhone)
