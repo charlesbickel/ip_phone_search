044c831127b7db60bcd48938bcbe8b1328948fd1
Charles Bickel <CBickel87@gmail.com>
2016-04-25 22:48:08 -0400

    Striped whitespace
    Striped whitespace from dict keys. Thank you Daniel :+1:

--- ip_phone_search.py
+++ ip_phone_search.py
@@ -20,16 +20,16 @@ def scrape(r):
     stuff = (doc.xpath('.//b/text()'))
 
     # Use dict comprehension to convert list into dict
-    d = dict([(k, v) for k, v in zip(stuff[::2], stuff[1::2])])
+    d = dict([(k.strip(), v) for k, v in zip(stuff[::2], stuff[1::2])])
 
     # Gather the dict values into variables
-    mac = (d.get(' MAC Address'))
-    hname = (d.get(' Host Name'))
-    serial = (d.get(' Serial Number'))
-    modelnum = (d.get(' Model Number'))
-    appload = (d.get(' App Load ID'))
-    bootload = (d.get(' Boot Load ID'))
-    hdware = (d.get(' Hardware Revision'))
+    mac = (d.get('MAC Address'))
+    hname = (d.get('Host Name'))
+    serial = (d.get('Serial Number'))
+    modelnum = (d.get('Model Number'))
+    appload = (d.get('App Load ID'))
+    bootload = (d.get('Boot Load ID'))
+    hdware = (d.get('Hardware Revision'))
 
     # Write data to csv file
     with open('IP_Phones.csv', 'a', newline='') as f:
@@ -79,6 +79,6 @@ print('The scan took', scan_time, 'seconds to run.')
 print(ip_phone, 'IP phones.')
 print(match_phones, 'Matching phones.')
 
-textMyself.textmyself('The task completed.{}IP phones. {} matching phones. '
+textMyself.textmyself('The task completed.{} IP phones. {} matching phones. '
                       'The scan took {} seconds to run.'.format(ip_phone,
                       match_phones, scan_time))
